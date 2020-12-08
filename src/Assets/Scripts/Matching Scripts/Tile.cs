db263950302d58195def7d29508c5ab467187a65
Andy <andyocampo@users.noreply.github.com>
2020-12-08 16:23:15 -0600

    Adds Source code

---
+++ src/Assets/Scripts/Matching Scripts/Tile.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private int column, row;
+    private bool isSelected;
+    public bool isMatched;
+    List<Tile> neighboringTiles = new List<Tile>();
+
+    private SpriteRenderer spriteRenderer;
+    private static Tile lastTileSelected = null; //the tile that the player will move
+
+    private Color colorWhenSelected = new Color(.5f, .5f, .5f, 1); //color that is given to sprite when selected
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        isMatched = false;
+    }
+
+    private void Start()
+    {
+        neighboringTiles = new List<Tile>();
+        column = (int)this.transform.position.x;
+        row = (int)this.transform.position.y;
+    }
+
+    private void Update()
+    {
+        if(isMatched)
+        {
+            spriteRenderer.color = new Color(1f, 1f, 1f, 0.25f);
+        }
+        if(isSelected == true)
+        {
+            Debug.DrawRay((Vector2)this.transform.position, Vector2.up, Color.black);
+            Debug.DrawRay((Vector2)this.transform.position, Vector2.down, Color.magenta);
+            Debug.DrawRay((Vector2)this.transform.position, Vector2.left, Color.red);
+            Debug.DrawRay((Vector2)this.transform.position, Vector2.right, Color.green);
+        }
+    }
+
+    //applies selected color
+    public void ApplySelectedColor()
+    {
+        spriteRenderer.color = colorWhenSelected;
+    }
+
+    //removes selected color
+    public void RemoveSelectedColor()
+    {
+        spriteRenderer.color = Color.white;
+    }
+
+    //makes tile selected and sets to current tile selected.
+    public void SelectTile()
+    {
+        isSelected = true;
+        ApplySelectedColor();
+        lastTileSelected = gameObject.GetComponent<Tile>(); //selected tile
+        Debug.Log($"Current tile is {lastTileSelected.name}");
+    }
+
+    //makes tile not selected and sets current tile selected to null.
+    public void DeselectTile()
+    {
+        isSelected = false;
+        RemoveSelectedColor();
+        lastTileSelected = null;
+        neighboringTiles.Clear();
+    }
+
+    //calculates which neighbors are next to new chosen tile
+    private void CalculateNeighbors()
+    {
+        if (this.column > 0)
+            neighboringTiles.Add(TileManager.Instance.allTiles[column - 1, row]);
+        if (this.column < TileManager.Instance.xWidth - 1)
+            neighboringTiles.Add(TileManager.Instance.allTiles[column + 1, row]);
+        if (this.row > 0)
+            neighboringTiles.Add(TileManager.Instance.allTiles[column, row - 1]);
+        if (this.row < TileManager.Instance.yHeight - 1)
+            neighboringTiles.Add(TileManager.Instance.allTiles[column, row + 1]);
+    }
+
+    //swaps sprites
+    public void SwapTiles(SpriteRenderer otherTileSprite)
+    {
+        if (spriteRenderer.sprite == otherTileSprite.sprite)
+        { return; }
+
+        Sprite newSprite = otherTileSprite.sprite;
+        otherTileSprite.sprite = spriteRenderer.sprite;
+        spriteRenderer.sprite = newSprite;
+    }
+
+    //when left mouse button is pressed over tile (will be moved to its own classed eventually)
+    private void OnMouseDown()
+    {
+        if (spriteRenderer.sprite == null)
+        { return; }
+
+        if (isSelected) //if current tile is selected, deselect on click
+        {
+            DeselectTile();
+        }
+        else
+        {
+            if (lastTileSelected == null) //if no tile has been selected, make this tile the first tile selected on click
+            {
+                SelectTile();
+                foreach (Tile go in neighboringTiles)
+                {
+                    Debug.Log(go.name);
+                }
+            }
+            else
+            {
+                CalculateNeighbors(); //calculates neighbors of new chosen tile
+                if (neighboringTiles.Contains(lastTileSelected)) //if last tile selected is a neighbor of the new chosen tile
+                {
+                    SwapTiles(lastTileSelected.spriteRenderer);
+                    MatchManager.Instance.FindMatches(); //checks if tile movement was match
+                    lastTileSelected.DeselectTile();
+                }
+                else
+                {
+                    lastTileSelected.DeselectTile();
+                    SelectTile();
+                }
+            }
+        }
+    }
+}
---
+++ src/Assets/Scripts/Matching Scripts/TileManager.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    private static TileManager instance;
+    public static TileManager Instance 
+    { 
+        get
+        {
+            if (instance == null)
+            {
+                Debug.LogError("TileManager does not exist!");
+            }
+
+            return instance;
+        }
+    }
+    public Tile[,] allTiles;
+    public int xWidth;  //amount of columns of tiles
+    public int yHeight; //amount of rows of tiles
+    public bool isRefilling;
+    [SerializeField] GameObject tilePrefab;
+    [SerializeField] Sprite[] tileSpriteTypes;
+    SpriteRenderer newTileRenderer;
+
+    private void Awake()
+    {
+        instance = this;
+        allTiles = new Tile[xWidth, yHeight];
+        CreateLevel();
+    }
+
+    //creates the level
+    private void CreateLevel()
+    {
+        Sprite[] previousTileBelow = new Sprite[xWidth];
+        Sprite previousTileLeft = null; 
+
+        for (int y = 0; y < yHeight; y++)
+        {
+            for (int x = 0; x < xWidth; x++)
+            {
+                RandomizeTile(x, y);
+
+                //checks whether a match was found during level creation
+                List<Sprite> possibleTiles = new List<Sprite>();
+                possibleTiles.AddRange(tileSpriteTypes);
+
+                possibleTiles.Remove(previousTileBelow[x]);
+                possibleTiles.Remove(previousTileLeft);
+
+                Sprite newSprite = possibleTiles[Random.Range(0, possibleTiles.Count)];
+                newTileRenderer.sprite = newSprite;
+                previousTileBelow[x] = newSprite;
+                previousTileLeft = newSprite;
+            }
+        }
+    }
+
+    //randomizes level with different sprites
+    private void RandomizeTile(int xPos, int yPos)
+    {
+        Vector2 startPosition = new Vector2(xPos, yPos);
+        GameObject tile = Instantiate(tilePrefab, startPosition, Quaternion.identity, this.transform);
+
+        Sprite randomTileSprite = tileSpriteTypes[Random.Range(0, tileSpriteTypes.Length)];
+        tile.GetComponent<SpriteRenderer>().sprite = randomTileSprite;
+        tile.name = tile.name.Replace("(Clone)", $"({xPos},{yPos})");
+        allTiles[xPos, yPos] = tile.GetComponent<Tile>();
+        newTileRenderer = tile.GetComponent<SpriteRenderer>();
+    }
+
+    //finds tiles with a null sprite and start refills
+    public IEnumerator FindNullTiles()
+    {
+        for (int x = 0; x < xWidth; x++)
+        {
+            for (int y = 0; y < yHeight; y++)
+            {
+                if (allTiles[x, y].GetComponent<SpriteRenderer>().sprite == null)
+                {
+                    yield return StartCoroutine(RefillTiles(x, y));
+                    break;
+                }
+            }
+        }
+
+        for (int x = 0; x < xWidth; x++)
+        {
+            for (int y = 0; y < yHeight; y++)
+            {
+                MatchManager.Instance.ClearAllMatches();
+            }
+        }
+    }
+
+    //refills null tile sprites
+    private IEnumerator RefillTiles(int x, int yStart)
+    {
+        isRefilling = true;
+        List<SpriteRenderer> renderer = new List<SpriteRenderer>();
+        int nullCount = 0;
+
+        for (int y = yStart; y < yHeight; y++)
+        {
+            SpriteRenderer spriteRenderer = allTiles[x, y].GetComponent<SpriteRenderer>();
+            if (spriteRenderer.sprite == null)
+            {
+                nullCount++;
+            }
+            renderer.Add(spriteRenderer);
+        }
+
+        for (int i = 0; i < nullCount; i++)
+        {
+            yield return new WaitForSeconds(0.3f);
+            for (int j = 0; j < renderer.Count - 1; j++)
+            {
+                renderer[j].sprite = renderer[j + 1].sprite;
+                renderer[j + 1].sprite = GetNewSprite(x, yHeight - 1);
+            }
+        }
+        isRefilling = false;
+    }
+
+    //gets a new sprite to refill
+    private Sprite GetNewSprite(int x, int y)
+    {
+        List<Sprite> tileSprites = new List<Sprite>();
+        tileSprites.AddRange(tileSpriteTypes);
+
+        if (x > 0)
+        {
+            tileSprites.Remove(allTiles[x - 1, y].GetComponent<SpriteRenderer>().sprite);
+        }
+        if (x < xWidth - 1)
+        {
+            tileSprites.Remove(allTiles[x + 1, y].GetComponent<SpriteRenderer>().sprite);
+        }
+        if (y > 0)
+        {
+            tileSprites.Remove(allTiles[x, y - 1].GetComponent<SpriteRenderer>().sprite);
+        }
+
+        return tileSprites[Random.Range(0, tileSprites.Count)];
+    }
+}
